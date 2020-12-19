87647cf772adeacbca33b3a6edae852c90a84fd5
Hanna <46329108+fafafafafafafafafafa@users.noreply.github.com>
2020-12-19 21:57:19 +0100

    Add files via upload
    the best I could

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
 import math
-
 import numpy
-import matplotlib
 import matplotlib.pyplot as plt
 
-# ................................................
-# generate random a, b and c for each parent in mi size population from unif distrib, -10<=a,b,c<=10
-# generate random sa, sb and sc for each parent in mi size population from unif distrib, 0<=sa,sb,sc<=10
-
-# A = [[1, 2, 3], [4, 5, 6]]
-# A = numpy.append(A, [[2, 2, 2]], axis=0)
-# ................................................
 f = open("ES_data_8.dat")
-# print(f.readlines())
 data = numpy.empty((0, 2), float)
 for line in f.read().splitlines(False):
-    # print(list(map(float, line.lstrip().split("  "))))
-    data = numpy.append(data, numpy.array([list(map(float, (line.lstrip().split("  "))))]), axis=0)
+    data = numpy.append(data, numpy.array([list(map(float, (line.lstrip().split())))]), axis=0)
+
+f.close()
 
 t = 0
+t_best = 0
 d = pow(10, -5)
 n = 6
 N = 101
-N_max = 200
+N_max = 100
 tau1 = 1/math.sqrt(2*n)
 tau2 = 1/math.sqrt(2*math.sqrt(n))
 mi_size = 1000
 lambda_size = mi_size * 5
 best_abcE = None
-# mi = [['a', 'b', 'c', 'sa', 'sb', 'sc']]
-# mi = numpy.append(mi, [['aa', 'bb', 'cc', 'saa', 'sbb', 'scc']], axis=0)
 
 
 def mse_calc(a, b, c):
@@ -44,8 +34,10 @@ def mse_calc(a, b, c):
 
 def parent_offspring_error():  # |E_o - E_p| <= d
     for i in range(mi_size):
+        if mi[i][6] > 0.25:
+            continue
         for j in range(6):
-            if abs(mi[i][6] - lambda_pop[i + j][6]) <= d and mi[i][6] <= 0.25 and lambda_pop[i + j][6] <= 0.25:
+            if abs(mi[i][6] - lambda_pop[i + j][6]) <= d:  # and mi[i][6] <= 0.25 and lambda_pop[i + j][6] <= 0.25:
                 return i+j
     return -1
 
@@ -59,8 +51,6 @@ for i in range(mi_size):  # make initial population: abc - unif dist val from -1
     c = numpy.random.uniform(-10, 10)
     sc = numpy.random.uniform(0, 10)
     mi = numpy.append(mi, [[a, b, c, sa, sb, sc, mse_calc(a, b, c)]], axis=0)
-
-# go back here after making new mi (???)
 
 for it in range(N_max):
     print("generation: ", t)
@@ -81,26 +71,41 @@ for it in range(N_max):
                                                     sa_offspring, sb_offspring, sc_offspring,
                                                     mse_calc(a_offspring, b_offspring, c_offspring)]], axis=0)
 
-
-
     # check E_o - E_p:
-    poe = parent_offspring_error()
-    if poe != -1:
+
+    mi_errors = numpy.take(mi, [6], axis=1)
+    min_mi_error_index = numpy.where(mi_errors == numpy.amin(mi_errors))
+    # print(min_mi_error_index[0][0]) # index of the best parent
+    lambda_pop_errors = numpy.take(lambda_pop, [6], axis=1)
+    min_lambda_pop_error_index = numpy.where(lambda_pop_errors == numpy.amin(lambda_pop_errors))
+    # print(min_lambda_pop_error_index[0][0])  #index of the best orrspring
+
+    #poe = parent_offspring_error()
+    #if poe != -1:
+    #    print('found!!!')
+    #    best_abcE = [lambda_pop[poe][0], lambda_pop[poe][1], lambda_pop[poe][2], lambda_pop[poe][6]]
+    #    break  # wylezc z petli
+
+    if abs(lambda_pop[min_lambda_pop_error_index[0][0]][6] - mi[min_mi_error_index[0][0]][6]) <= d:
+        best_child = lambda_pop[min_lambda_pop_error_index[0][0]]
         print('found!!!')
-        best_abcE = [lambda_pop[poe][0], lambda_pop[poe][1], lambda_pop[poe][2], lambda_pop[poe][6]]
-        break  # wylezc z petli
+        # best_abcE = [best_child[0], best_child[1], best_child[2], best_child[6]]
+        # break  # wylezc z petli
 
     mi_and_lambda = numpy.append(mi, lambda_pop, axis=0)
     mi_and_lambda.view('i8, i8, i8, i8, i8, i8, i8').sort(order=['f6'], axis=0)
-    mi = mi_and_lambda[0:mi_size]
-    best_abcE = [mi[0][0], mi[0][1], mi[0][2], mi[0][6]]
+    mi = mi_and_lambda[0: mi_size]
+
+    if best_abcE is None or mi[0][6] < best_abcE[3]:
+        best_abcE = [mi[0][0], mi[0][1], mi[0][2], mi[0][6]]
+        t_best = t
     t = t + 1
 
 print("Best [a,b,c] = [" + str(best_abcE[0]) +', ' + str(best_abcE[1]) +', ' + str(best_abcE[2]) + ']')
 print("MSE = " + str(best_abcE[3]))
 print("iteration: " + str(t))
 
-# plot of data 8:
+# plot of data 8 or 9:
 fig, ax = plt.subplots()
 ax.plot(numpy.take(data, [0], axis=1), numpy.take(data, [1], axis=1))  # i o
 ax.set(xlabel='input', ylabel='output',
@@ -110,17 +115,15 @@ fig.savefig("data8.png")
 
 # plot of data o_hat vs i
 fig, ax = plt.subplots()
-# ax.plot(numpy.take(data, [0], axis=1), best_abcE[0] * (numpy.square(row[0]) - best_abcE[1]*numpy.cos(best_abcE[1] * numpy.pi * row[0])))  # i o
 ax.plot(numpy.take(data, [0], axis=1), [best_abcE[0] * (numpy.square(row[0]) - best_abcE[1]*math.cos(best_abcE[1] * numpy.pi * row[0])) for row in numpy.take(data, [0], axis=1)])
 ax.set(xlabel='input', ylabel='output',
        title='o_hat vs input')
 ax.grid()
-fig.savefig("out.png")
+fig.savefig("out_8.png")
 
 
 
 # todo:
-#  calculate fitness val for every new individual
-#  combining mi and lambda_pop, pick the new mi
-#  change the t = t+1, if t<N_max -> again generate mutants etc. (loop; Nmax = 200)
-#  use matplot to make plot of o = f(i) and O_hat = (formula)
+#  ?????
+#  what next?
+#  it is very slow
